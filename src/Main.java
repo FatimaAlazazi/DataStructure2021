352716552c84417264d187b9837c34c91bf64a6b
fatima <f.alazazi1989@gmail.com>
2021-01-09 19:25:32 +0300

    third update

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Word");
         System.out.println("Fatima");
+        System.out.println("third update");
     }
 }
